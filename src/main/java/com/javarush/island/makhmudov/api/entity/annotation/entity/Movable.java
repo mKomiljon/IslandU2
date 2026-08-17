5452fef6b47d9b4675aaa89b537156c67b143097
K.Makhmudov <komiljonmakhmudov76@gmail.com>
2026-08-17 06:06:39 +0500

    Asta ikkinchi

--- src/main/java/com/javarush/island/makhmudov/api/entity/annotation/entity/Movable.java
+++ src/main/java/com/javarush/island/makhmudov/api/entity/annotation/entity/Movable.java
@@ -1,7 +1,8 @@
 package com.javarush.island.makhmudov.api.entity.annotation.entity;
 
-import com.javarush.island.khmelov.entity.map.Cell;
+import com.javarush.island.makhmudov.entity.map.Cell;
 
+@FunctionalInterface
 public interface Movable {
     boolean move(Cell startCell);
 }

ba0da5f32557e52200eab3b1475c624de800ce65
K.Makhmudov <komiljonmakhmudov76@gmail.com>
2026-08-18 19:10:54 +0500

    Asta 13 oldingisida servislayam qilinudi endi runnerla

---
+++ src/main/java/com/javarush/island/makhmudov/view/JavaFxRunner.java
@@ -0,0 +1,31 @@
+package com.javarush.island.makhmudov.view;
+
+import com.javarush.island.makhmudov.api.init.Initialization;
+import com.javarush.island.makhmudov.config.Setting;
+import com.javarush.island.makhmudov.entity.Game;
+import com.javarush.island.makhmudov.entity.map.GameMap;
+import com.javarush.island.makhmudov.repository.EntityCreator;
+import com.javarush.island.makhmudov.repository.GameMapCreator;
+import com.javarush.island.makhmudov.services.*;
+
+import java.util.List;
+
+public class JavaFxRunner {
+    public static void main(String[] args) {
+        Initialization entityFactory = new EntityCreator();
+        GameMapCreator gameMapCreator = new GameMapCreator(entityFactory);
+        int rows = Setting.get().life.getRows();
+        int cols = Setting.get().life.getCols();
+        GameMap gameMap = gameMapCreator.createRandomFilledGameMap(rows, cols, 1);
+        Game game = new Game(gameMap, entityFactory, null);
+        List<Runnable> services = List.of(
+                new EatingService(game),
+                new MovingService(game),
+                new ReproducingService(game),
+                new RandomFillService(game),
+                new ViewService(game)
+        );
+        GameServiceProcessor gameWorkerService = new GameServiceProcessor(game, services);
+        JavaFxView.launchFxWindow(gameWorkerService);
+    }
+}
